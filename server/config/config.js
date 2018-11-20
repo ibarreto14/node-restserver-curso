bc4fed0d97168b6c2ca7d6f59246c30f57c59a91
Ivan Barrett <lrangel.slash@gmail.com>
2018-11-20 13:56:27 -0600

    RestServer - usuaario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
 
 // =======================================
-// PUERTO
+// PUERTO NODE
 // =======================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =======================================
+// ENTORNO
+// =======================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======================================
+// DB
+// =======================================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+	urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+	urlDB = 'mongodb://cafe-user:m13ntr4s@ds153705.mlab.com:53705/cafe';
+}
+
+process.env.URLDB = urlDB;
